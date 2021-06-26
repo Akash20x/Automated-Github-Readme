ca6ab85e373d459e0678d5fe7ef7ff8502c8a788
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-06-27 02:06:03 +0530

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     file_names = Exract_files_names()
     file_md="\n\n".join(["- {}".format(i) for i in file_names])
     file_md = "\n\n".join(
-        ["| [{fname}]({furl}) |           |".format(**i) for i in file_names]
+        ["- [{fname}]({furl})".format(**i) for i in file_names]
     )
 
     readme_contents = replace_chunk(readme_contents, "Projects", file_md)
