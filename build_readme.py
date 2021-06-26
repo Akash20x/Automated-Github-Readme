d6b17799493413dc7899f5b29952720f0630e242
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-06-27 01:55:04 +0530

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -2,11 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import pathlib
 import re
-import requests
 
 
 ROOT_PATH = pathlib.Path(__file__).parent.resolve()
-FEED_URL = 'https://github.com/Akash20x/Automated-Github-Readme'
+FEED_URL = 'https://github.com/Niketkumardheeryan/Hands-on-ML-Basic-to-Advance-'
 
 def replace_chunk(content, marker, chunk, inline=False):
     r = re.compile(
@@ -27,15 +26,13 @@ def Exract_files_names():
 	li = soup.findAll('div', class_="Box-row Box-row--focus-gray py-2 d-flex position-relative js-navigation-item")
 	for i in li:
 		for x in i.findAll('a',class_="js-navigation-open Link--primary"):
-			if(x.text!=".github" and x.text!="CODE_OF_CONDUCT.md" 
-				and x.text!="CONTRIBUTING_GUIDELINES.md" and x.text!=".github/workflows" and x.text!="build_readme.py" and and x.text!="requirements.txt"
-				x.text!="README.md" and x.text!="download statistics.jpg" 
-				and x.text!="img" and x.text!="ml img.jpg"):
-
-				repo_name = x.text
-				temp.append(repo_name)
-			else:
-				pass
+			if(x.text!=".github" and x.text!="CODE_OF_CONDUCT.md" and x.text!="CONTRIBUTING_GUIDELINES.md" and x.text!=".github/workflows" and x.text!="build_readme.py" and x.text!="requirements.txt" and x.text!="README.md" and x.text!="download statistics.jpg" and x.text!="img" and x.text!="ml img.jpg"):
+				temp2={
+                	'fname' : x.text,
+                	'furl': x["href"]
+				   		}
+				temp.append(temp2)
+			else:pass
 	return temp
 
 
@@ -45,9 +42,13 @@ if __name__ == "__main__":
 
     file_names = Exract_files_names()
     file_md="\n\n".join(["- {}".format(i) for i in file_names])
+    file_md = "\n\n".join(
+        ["- [{fname}]({furl})".format(**i) for i in file_names]
+    )
 
     readme_contents = replace_chunk(readme_contents, "Projects", file_md)
     readme.open("w").write(readme_contents)
+
 
 
 
