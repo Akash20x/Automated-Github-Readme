ec0db10e59331e6bc520fa5f847d110939d1c9cc
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-06-29 18:28:50 +0530

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -5,7 +5,7 @@ import re
 
 
 ROOT_PATH = pathlib.Path(__file__).parent.resolve()
-FEED_URL = 'https://github.com/Akash20x/Automated-Github-Readme'
+FEED_URL = 'https://github.com/Niketkumardheeryan/Hands-on-ML-Basic-to-Advance-'
 
 def replace_chunk(content, marker, chunk, inline=False):
     r = re.compile(
